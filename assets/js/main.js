ba413784443b8a0c982bf14cf137b6a304f3d2cc
Guillaume PERE <guillaume.pere@orange.com>
2024-03-06 15:47:21 +0100

    Add a welcome page + manage scores tabs

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+function setPseudo(pseudo) {
+  localStorage.setItem('pseudo', pseudo);
+}
+
+function getPseudo() {
+  return localStorage.getItem('pseudo');
+}
+
+function setVersion(time) {
+  localStorage.setItem('version', time);
+}
+
+function getVersion() {
+  return localStorage.getItem('version');
+}
