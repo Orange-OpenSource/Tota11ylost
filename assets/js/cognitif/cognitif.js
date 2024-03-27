795d618790323bf7f43820ae59d8b632efadeeb4
Guillaume PERE <guillaume.pere@orange.com>
2024-03-27 16:19:58 +0100

    wording : i18n of the project

--- assets/js/cognitif/cognitif.js
+++ assets/js/cognitif/cognitif.js
@@ -1,38 +1,42 @@
-const linkDyslexie = [
-  {
-    label: "oui c'est sur, ce lien est le bon",
-    href: "./3-1-simulation-cognitive.html"
-  },
-  {
-    label: "oui bien sur, avec du jambon",
-    href: "./3-cognitif"
-  },
-  {
-    label: "oui c'est dur, ce lien est un rond",
-    href: "./3-cognitif"
-  },
-  {
-    label: "oui c'est nul, tien un bonbon",
-    href: "./3-cognitif"
-  }
-];
-
 function shuffle(array) {
   array.sort(() => Math.random() - 0.5);
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
-  let divLink = document.getElementById('linkDyslexia');
-  shuffle(linkDyslexie)
-  linkDyslexie.forEach(linkD => {
-    let button = document.createElement('button');
-    button.textContent = linkD.label;
-    button.classList.add("btn", "btn-primary", "btn-lg", "m-2", "dyslexia");
-    button.setAttribute("role", "link");
-    button.addEventListener("click", (e) => {
-      document.location.assign(linkD.href);
-    });
+  langManager.subscribe(
+    () => {
+      let divLink = document.getElementById('linkDyslexia');
 
-    divLink.append(button)
-  });
+      const linkDyslexie = [
+        {
+          label: i18next.t('cognitif.buttonsLabels', {returnObjects: true})[0],
+          href: "./3-1-simulation-cognitive.html"
+        },
+        {
+          label: i18next.t('cognitif.buttonsLabels', {returnObjects: true})[1],
+          href: "./3-cognitif"
+        },
+        {
+          label: i18next.t('cognitif.buttonsLabels', {returnObjects: true})[2],
+          href: "./3-cognitif"
+        },
+        {
+          label: i18next.t('cognitif.buttonsLabels', {returnObjects: true})[3],
+          href: "./3-cognitif"
+        }
+      ];
+
+      shuffle(linkDyslexie)
+      linkDyslexie.forEach(linkD => {
+        let button = document.createElement('button');
+        button.textContent = linkD.label;
+        button.classList.add("btn", "btn-primary", "btn-lg", "m-2", "dyslexia");
+        button.setAttribute("role", "link");
+        button.addEventListener("click", (e) => {
+          document.location.assign(linkD.href);
+        });
+
+        divLink.append(button)
+      });
+    });
 });
---
+++ lang/lang.js
@@ -0,0 +1,96 @@
+const langManager = function () {
+  let tabFn = [];
+  let langLoaded = false;
+
+  console.log(' langManager');
+  return {
+    subscribe: (fn) => {
+      if (langLoaded) {
+        setTimeout(() => {
+          fn();
+        });
+      } else {
+        tabFn.push(fn);
+      }
+      return tabFn.filter(() => {
+      });
+    },
+
+    onLangLoaded: () => {
+      langLoaded = true;
+      // Call all pending functions
+      while (tabFn.length > 0) {
+        // Remove the first function from the array
+        const func = tabFn.shift();
+        // call the function
+        func();
+      }
+    },
+
+    langReset: () => {
+      langLoaded = false;
+    }
+  };
+}();
+
+
+i18next
+  .use(i18nextHttpBackend)
+  .init({
+    lng: 'fr', // Langue par défaut
+    fallbackLng: 'en', // Langue de secours
+    debug: true,
+    backend: {
+      loadPath: './locales/{{lng}}.json'
+    },
+  }, function (err, t) {
+    changeLanguage(getLang());
+    langManager.onLangLoaded();
+  });
+
+function updateTranslations() {
+  const elements = document.querySelectorAll('[data-i18n]');
+  elements.forEach(function (element) {
+    const keys = element.getAttribute('data-i18n').split(';');
+    keys.forEach(function (key) {
+      const attr = getAttributeFromKey(key);
+      if (attr !== -1) {
+        const value = i18next.t(key);
+        element.setAttribute(attr, value);
+      } else {
+        element.innerHTML = i18next.t(key);
+      }
+    });
+  });
+}
+
+function getAttributeFromKey(key) {
+  const elements = key.split('.');
+  const lastWord = elements[elements.length - 1];
+  const parts = lastWord.split('_');
+  return parts.length > 1 ? parts[0] : -1;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const languageSelect = document.getElementById('languageSelect');
+  languageSelect && languageSelect.addEventListener('change', function () {
+    changeLanguage(this.value);
+  });
+});
+
+function changeLanguage(lang) {
+  langManager.langReset();
+  i18next.changeLanguage(lang, () => {
+    setLang(lang);
+    langManager.onLangLoaded();
+    updateTranslations();
+  });
+}
+
+function setLang(lang) {
+  localStorage.setItem('lang', lang);
+}
+
+function getLang() {
+  return localStorage.getItem('lang');
+}
