60abdb5a0aa289abecfb2f25ae852baa8b91b544
Guillaume PERE <guillaume.pere@orange.com>
2024-03-28 16:02:44 +0000

    New header + correction in CognitiveSimu page

--- assets/js/cognitif/cognitif31.js
+++ assets/js/cognitif/cognitif31.js
@@ -7,7 +7,7 @@ validButton.addEventListener("click", (e) => {
   const second = document.getElementById('deuxieme').value.toLowerCase();
   const third = document.getElementById('troisieme').value.toLowerCase();
 
-  const listOfResponses = i18next.t('cognitifSimu.listOfResponses', {returnObjects: true});
+  const listOfResponses = i18next.t('cognitiveSimu.listOfResponses', {returnObjects: true});
 
   if (first.indexOf(listOfResponses[0]) > -1 && second.indexOf(listOfResponses[1]) > -1 && third.indexOf(listOfResponses[2]) > -1) {
     document.location.assign(href);
