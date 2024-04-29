8830bd352902b5d4a49ba12aa290677ce6ca380e
MewenLeHo <mewen.leho.ext@orange.com>
2024-04-29 18:22:22 +0200

    Add hints in physical page

--- assets/js/hints/_physical.js
+++ assets/js/hints/_physical.js
@@ -12,5 +12,13 @@ btnIndice.addEventListener("click", (e) => {
 });
 
 function stopMouse() {
-  console.log("bidule");
+  const falseCursor = document.getElementById("falsecursor");
+  const documentBody = document.querySelectorAll("body");
+  const closePopup = document.getElementById("close-popup");
+  const validateLink = document.getElementById("link30or60");
+
+  falseCursor.style.display = "none";
+  documentBody[0].classList.remove("no-cursor");
+  closePopup.style.cursor = "pointer";
+  validateLink.style.cursor = "pointer";
 }
