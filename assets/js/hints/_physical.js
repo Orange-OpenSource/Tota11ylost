f2c5492ff2b3ec05d749ff1b0694f2a767c4e146
MewenLeHo <mewen.leho.ext@orange.com>
2024-04-29 16:30:47 +0200

    Add hints in physical page WIP

---
+++ assets/js/hints/_physical.js
@@ -0,0 +1,16 @@
+btnIndice.addEventListener("click", (e) => {
+  switch (indice) {
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      stopMouse();
+      break;
+    default:
+  }
+});
+
+function stopMouse() {
+  console.log("bidule");
+}
