0eb6c24ad54e6441c82da1c8d711817357fa1356
MewenLeHo <mewen.leho.ext@orange.com>
2024-05-13 11:22:38 +0200

    Complete refactoring of hints in physical simulation page

--- assets/js/hints/_physical.js
+++ assets/js/hints/_physical.js
@@ -12,6 +12,11 @@ btnIndice.addEventListener("click", (e) => {
 });
 
 function stopMouse() {
+  document.removeEventListener("DOMContentLoaded", setFalseCursor);
+  clearInterval(intervalTremorFalseCursor);
+  document.removeEventListener("mousemove", addMouseEvent);
+  document.removeEventListener("click", addClickEvent);
+
   const falseCursor = document.getElementById("falsecursor");
   const documentBody = document.querySelectorAll("body");
   const closePopup = document.getElementById("close-popup");
@@ -21,4 +26,12 @@ function stopMouse() {
   documentBody[0].classList.remove("no-cursor");
   closePopup.style.cursor = "pointer";
   validateLink.style.cursor = "pointer";
+
+  function closeModal() {
+    const modalMoteur = document.getElementById("modalMoteur");
+    let modal = boosted.Modal.getInstance(modalMoteur);
+    modal.hide();
+  }
+
+  closePopup.addEventListener("click", closeModal);
 }
