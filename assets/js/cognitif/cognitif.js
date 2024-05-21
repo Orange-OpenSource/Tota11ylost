3a8260ebaea9359eb1aecff5aa9722da7ef6f611
Mewen Le Ho <mewen.leho.ext@orange.com>
2024-05-21 08:49:38 +0000

    Clues system refactoring

--- assets/js/cognitif/cognitif.js
+++ assets/js/cognitif/cognitif.js
@@ -1,42 +1,49 @@
+let divLink = document.getElementById("linkDyslexia");
+var globalLinkDyslexie;
+
 function shuffle(array) {
   array.sort(() => Math.random() - 0.5);
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
-  langManager.subscribe(
-    () => {
-      let divLink = document.getElementById('linkDyslexia');
+  langManager.subscribe(() => {
 
-      const linkDyslexie = [
-        {
-          label: i18next.t('cognitive.buttonsLabels', {returnObjects: true})[0],
-          href: "./cognitive-simulation.html"
-        },
-        {
-          label: i18next.t('cognitive.buttonsLabels', {returnObjects: true})[1],
-          href: "./cognitive.html"
-        },
-        {
-          label: i18next.t('cognitive.buttonsLabels', {returnObjects: true})[2],
-          href: "./cognitive.html"
-        },
-        {
-          label: i18next.t('cognitive.buttonsLabels', {returnObjects: true})[3],
-          href: "./cognitive.html"
-        }
-      ];
+    const linkDyslexie = [
+      {
+        label: i18next.t("cognitive.buttonsLabels", { returnObjects: true })[0],
+        href: "./cognitive-simulation.html",
+      },
+      {
+        label: i18next.t("cognitive.buttonsLabels", { returnObjects: true })[1],
+        href: "./cognitive.html",
+      },
+      {
+        label: i18next.t("cognitive.buttonsLabels", { returnObjects: true })[2],
+        href: "./cognitive.html",
+      },
+      {
+        label: i18next.t("cognitive.buttonsLabels", { returnObjects: true })[3],
+        href: "./cognitive.html",
+      },
+    ];
 
-      shuffle(linkDyslexie)
-      linkDyslexie.forEach(linkD => {
-        let button = document.createElement('button');
-        button.textContent = linkD.label;
-        button.classList.add("btn", "btn-primary", "m-2", "dyslexia", "fs-3", "p-2");
-        button.setAttribute("role", "link");
-        button.addEventListener("click", (e) => {
-          document.location.assign(linkD.href);
-        });
+    shuffle(linkDyslexie);
+    createButtons(linkDyslexie);
 
-        divLink.append(button)
-      });
-    });
+    globalLinkDyslexie = linkDyslexie;
+  });
 });
+
+function createButtons(array) {
+  array.forEach((linkD) => {
+    let button = document.createElement("button");
+    button.textContent = linkD.label;
+    button.classList.add("btn", "btn-primary", "m-2", "dyslexia", "fs-3", "p-2");
+    button.setAttribute("role", "link");
+    button.addEventListener("click", (e) => {
+      document.location.assign(linkD.href);
+    });
+
+    divLink.append(button);
+  });
+}
---
+++ assets/js/hints/hints.js
@@ -0,0 +1,121 @@
+const btnIndice = document.getElementById("indice");
+const displayTimer = document.getElementById("displayTimer");
+const hintsAvailable = document.getElementById("hintsAvailable");
+
+const pageID = document.getElementById("pageId").value;
+
+// Display timer before hint button activation
+function startTimer(duration, display) {
+  let start = Date.now(),
+    diff,
+    minutes,
+    seconds;
+
+  function timer() {
+    diff = duration - (((Date.now() - start) / 1000) | 0);
+
+    minutes = (diff / 60) % 60 | 0;
+    seconds = diff % 60 | 0;
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = " " + minutes + ":" + seconds;
+
+    if (diff <= 0) {
+      start = Date.now() + 1000;
+    }
+  }
+
+  timer();
+  setInterval(timer, 1000);
+}
+
+let display = document.querySelector("#hint-timer");
+
+startTimer(5 * 60, display);
+
+// Add param '?debug=true' at the end of the URL to reduce timer duration to 10s
+let params = new URLSearchParams(document.location.search);
+let debug = params.get("debug");
+let hintTimer = !debug ? 300000 : 10000;
+
+// Activate hint buttton and remove time after 5min
+setTimeout(() => {
+  btnIndice.removeAttribute("disabled");
+  displayTimer.classList.add("d-none");
+  hintsAvailable.classList.remove("d-none");
+}, hintTimer);
+
+// Hint system
+let indice = 0;
+
+const indiceTime = [30, 120, 300, 0];
+
+btnIndice.addEventListener("click", (e) => {
+  addTime(indiceTime[indice]);
+  indice++;
+  updateIndiceButton();
+
+  switch (indice) {
+    case 1:
+      addIndice();
+      break;
+    case 2:
+      addIndice();
+      break;
+    case 3:
+      addIndice();
+      e.target.disabled = true;
+      e.target.innerHTML = i18next.t("hints.noMoreHints");
+      break;
+    default:
+  }
+});
+
+function addIndice() {
+  const indiceDiv = document.getElementById("indice" + indice);
+
+  let para = document.createElement("p");
+  let node = document.createTextNode(
+    indice < 1
+      ? ""
+      : i18next.t("hints." + pageID, { returnObjects: true })[indice - 1]
+  );
+
+  para.appendChild(node);
+  indiceDiv.appendChild(para);
+  indiceDiv.classList.remove("d-none");
+}
+
+function updateIndiceButton() {
+  let getIndiceTime = indiceTime[indice];
+  let textTime = "";
+  let durationTime = "";
+
+  if (getIndiceTime < 60) {
+    textTime = "sec";
+    durationTime = getIndiceTime;
+  } else {
+    textTime = "min";
+    durationTime = getIndiceTime / 60;
+  }
+  btnIndice.textContent = i18next.t("form.labelHintsButton", {
+    duration: durationTime,
+    unit: textTime,
+  });
+}
+
+// JS hack needed for Firefox in order to reset btnIndice to disabled state on soft refresh
+// For reference, see https://stackoverflow.com/questions/5985839/bug-with-firefox-disabled-attribute-of-input-not-resetting-when-refreshing
+window.addEventListener("pageshow", PageShowHandler, false);
+window.addEventListener("unload", UnloadHandler, false);
+
+function PageShowHandler() {
+  btnIndice.setAttribute("disabled", true);
+  window.addEventListener("unload", UnloadHandler, false);
+}
+
+function UnloadHandler() {
+  window.removeEventListener("unload", UnloadHandler, false);
+}
