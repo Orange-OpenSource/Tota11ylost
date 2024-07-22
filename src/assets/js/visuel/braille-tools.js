efcd2a63e1011d3b352cc3d655c4727c98ef1b67
Guillaume PERE <guillaume.pere@orange.com>
2024-07-22 16:25:16 +0200

    remove wrong header

--- src/assets/js/visuel/braille-tools.js
+++ src/assets/js/visuel/braille-tools.js
@@ -1,17 +1,4 @@
 /*
-Software Name: Tota11ylost
-SPDX-FileCopyrightText: Copyright (c) Orange SA
-SPDX-License-Identifier: AGPL-3.0-or-later
-
-This software is distributed under the GNU Affero General Public License v3.0 or later,
-the text of which is available at https://opensource.org/license/agpl-v3
-or see the "LICENSE" file for more details.
-
-Authors: See CONTRIBUTORS.txt file
-Software description: Experience in a playful way the challenges faced by people with digital disabilities
-*/
-
-/*
   Braille-Tools
   (c) 2016 Olivier Giulieri
   https://github.com/evoluteur/braille-tools
