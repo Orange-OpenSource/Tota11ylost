dcb8117ff9aa57be193457c0ec427e04c4862af4
Guillaum31 <Guillaum31@users.noreply.github.com>
2024-07-23 13:22:33 +0000

    deploy: 20f70002dd020154677613bcc9b4406df0bcb01b

--- lang/lang.js
+++ lang/lang.js
@@ -53,7 +53,7 @@ i18next
     fallbackLng: 'en', // Langue de secours
     debug: true,
     backend: {
-      loadPath: '../locales/{{lng}}.json'
+      loadPath: '/locales/{{lng}}.json'
     },
   }, function (err, t) {
     changeLanguage(getLang() || 'fr');
