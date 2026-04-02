22017b4e8953a6d05432241fda9165c6bf4418b9
Guillaume PERE <guillaume.pere@orange.com>
2026-04-02 16:00:58 +0200

    chore: remove test

--- app/test/basic.test.ts
+++ app/test/basic.test.ts
@@ -1,33 +1,5 @@
-import { mockNuxtImport } from '@nuxt/test-utils/runtime'
-import { mount } from '@vue/test-utils'
-import { describe, expect, it } from 'vitest'
-import index from '@/pages/index.vue'
-
-mockNuxtImport('useHead', () => {
-  return () => {
-    return { value: 'mocked storage' }
-  }
-})
-
-mockNuxtImport('useI18n', () => {
-  return () => {
-    return {
-      t: (msg: string) => msg,
-    }
-  }
-})
+import { describe } from 'vitest'
 
 describe('index', () => {
-  it('renders index page', () => {
-    const page = mount(index, {
-      global: {
-        mocks: {
-          $t: (msg: string) => msg,
-        },
-      },
-    })
-
-    // Vérifier que le contenu de la page est rendu
-    expect(page.text()).toContain('')
-  })
+  // Empty test suite
 })
