26a4e9291531392097f03fe54697c9df8d458db5
Guillaume PERE <guillaume.pere@orange.com>
2026-04-14 17:09:50 +0200

    chore: add indent rules for the project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,8 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt({
   rules: {
     'vue/max-attributes-per-line': ['warn', { singleline: 3, multiline: 1 }],
+    '@stylistic/indent': 'off',
+    'vue/html-indent': ['error', 2],
+    'vue/script-indent': ['error', 2, { baseIndent: 0 }],
   },
 })
