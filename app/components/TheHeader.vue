c608329c31e13911461641b7c901d70b7415b7b5
Clara Marchal <marchal.clara95@gmail.com>
2026-04-20 12:09:41 +0200

    fix: nav lang no responsive

--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -3,8 +3,8 @@
     <!-- Language switcher at the top -->
     <LanguageSwitch />
     <div class="bg-dark py-2">
-      <div class="m-3 d-flex justify-content-between align-items-center">
-        <div class="d-flex align-items-end gap-3">
+      <div class="m-3 d-flex flex-row justify-content-between align-items-center flex-nowrap">
+        <div class="d-flex align-items-end gap-3 flex-nowrap">
           <NuxtLink to="/" :aria-label="$t('common.banner.alt_orangeLogo')">
             <img
               src="~/assets/img/orange-logo.png"
@@ -13,12 +13,14 @@
               style="max-height: 60px;"
             >
           </NuxtLink>
-          <h1 class="fs-2 m-0 text-white">
+          <h1 class="fs-2 m-0 text-white text-nowrap">
             {{ $t('common.banner.title') }}
           </h1>
         </div>
         <!-- Timer Display -->
-        <GameTimer />
+        <div class="flex-shrink-0">
+          <GameTimer />
+        </div>
       </div>
     </div>
   </header>
